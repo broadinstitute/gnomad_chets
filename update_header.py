2bcddc6447fd094ccf56dd8a25996e4c8040bc9d
konradjk <konradjkarczewski@gmail.com>
2016-11-16 00:22:18 -0500

    AN POPMAX is Number=A

--- update_header.py
+++ update_header.py
@@ -27,7 +27,7 @@ additional_descriptions = {
     'SOR': 'Strand Odds Ratio',
     'POPMAX': ('Population with max AF', 'A'),
     'AC_POPMAX': ('AC in the population with the max AF', 'A'),
-    'AN_POPMAX': ('AN in the population with the max AF', '1'),
+    'AN_POPMAX': ('AN in the population with the max AF', 'A'),
     'AF_POPMAX': ('Maximum Allele Frequency across populations (excluding OTH)', 'A'),
     'AB_HIST': ('Histogram for Allele Balance; 100*AD[i_alt]/sum(AD); Mids: 2.5|7.5|12.5|17.5|22.5|27.5|32.5|37.5|42.5|47.5|52.5|57.5|62.5|67.5|72.5|77.5|82.5|87.5|92.5|97.5', 'R'),
     'Hom': 'Count of homozygous individuals',
