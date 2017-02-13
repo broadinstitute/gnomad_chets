f804b9004b913fe4f0f6bfba682bbd3e53c4f24b
laurent <lfran@broadinstitute.org>
2017-02-12 23:45:22 -0500

    * Loop over chrom 22 * Uses new keep meta * Added culprit annotation renaming * Added annotation attributes

--- hail/genomes_sites_vcf.py
+++ hail/genomes_sites_vcf.py
@@ -8,7 +8,7 @@ from resources import *
 vds_path = "gs://gnomad/gnomad.10ksites.vds"
 meta_path = "gs://gnomad/gnomad.final.all_meta.txt"
 vep_path = "gs://gnomad/gnomad.splitmulti.vep.vds"
-rf_path = "gs://gnomad/RF/gnomad.sites.RF.newStats12.vds"
+rf_path = "gs://gnomad/RF/gnomad.sites.RF.newStats24.vds"
 vep_config = "/vep/vep-gcloud.properties"
 
 # Resources
@@ -16,7 +16,7 @@ autosomes_intervals = "gs://gnomad/autosomes.txt"
 
 # Outputs
 date_time = time.strftime("%Y-%m-%d_%H-%M")
-#date_time = '2017-01-30_22-18'
+date_time = '2017-02-13_01-36'
 #date_time = '2016-12-20_21-20'
 out_root = "gs://gnomad-lfran/tmp"
 out_vds_prefix = "%s/gnomad.sites.annotations.%s" % (out_root, date_time)
@@ -30,15 +30,15 @@ intervals_tmp = '/tmp'
 pops = ['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH']
 
 #Actions
-preprocess_autosomes = True
+preprocess_autosomes = False
 postprocess_autosomes = True
 write_autosomes = True
-preprocess_X = True
-postprocess_X = True
-write_X = True
-preprocess_Y = True
-postprocess_Y = True
-write_Y = True
+preprocess_X = False
+postprocess_X = False
+write_X = False
+preprocess_Y = False
+postprocess_Y = False
+write_Y = False
 
 
 hc = HailContext(log='/site_auto.log')
@@ -52,10 +52,12 @@ def preprocess_vds(vds_path):
             .annotate_samples_table(meta_path, 'Sample', root='sa.meta', config=hail.TextTableConfig(impute=True))
             .annotate_samples_expr(['sa.meta.population = sa.meta.final_pop',
                                     'sa.meta.project_description = sa.meta.Title'])  # Could be cleaner
-            .filter_samples_expr('!isMissing(sa.meta.predicted_pop)')  # Could be cleaner
+            .filter_samples_expr('sa.meta.keep')
             .filter_variants_intervals('gs://gnomad-lfran/tmp/test.interval')
             .annotate_variants_intervals(decoy_path, 'va.decoy')
             .annotate_variants_intervals(lcr_path, 'va.lcr')
+            .annotate_variants_expr('va.info.VQSR_culprit = va.info.culprit')
+            .annotate_variants_expr('va.info = drop(va.info, culprit)')
     )
 
 
@@ -68,13 +70,15 @@ def post_process_vds(vds_path):
         'SEGDUP' : 'va.decoy',
         'LCR': 'va.lcr'
     }
+    vds = hc.read(vds_path)
 
-    return (
-            set_vcf_filters(hc, vds_path, rf_path, 'va.RF1', 0.5, 0.75, filters=filters,
-                        filters_to_keep=['InbreedingCoefficient'], tmp_path='/tmp')
-            .annotate_variants_vds(vep, code='va.info.CSQ = vds.csq')
-            .vep(config=vep_config, csq=True, root='va.info.CSQ')
-    )
+    #vds = set_vcf_filters(hc, vds_path, rf_path, 'va.RF1', 0.4, 0.4, filters=filters,
+    #                    filters_to_keep=['InbreedingCoefficient'], tmp_path='/tmp')
+    vds = vds.vep(config=vep_config, csq=True, root='va.info.CSQ', force=True)
+    vds = set_va_attributes(vds)
+
+    return (vds)
+#.annotate_variants_vds(vep, code='va.info.CSQ = vds.csq')
 
 
 def write_vcfs(vds_path, contig):
@@ -108,7 +112,7 @@ if postprocess_autosomes:
     post_process_vds(tmp_vds_prefix + ".vds").write(out_vds_prefix + ".vds")
 
 if write_autosomes:
-    for i in range(1,22):
+    for i in range(1,23):
         write_vcfs(out_vds_prefix + ".vds", i)
 
 if preprocess_X:
