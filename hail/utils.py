faf7833304bfb35310b92c49db418173482e95a4
laurent <lfran@broadinstitute.org>
2017-02-15 04:37:48 -0500

    Added HaplotypeScore to list of fields to remove

--- hail/utils.py
+++ hail/utils.py
@@ -472,7 +472,7 @@ def common_sites_vds_annotations(vds):
         vds.annotate_variants_expr(['va.info.VQSR_culprit = va.info.culprit',
                                     'va.info.VQSR_NEGATIVE_TRAIN_SITE = va.info.NEGATIVE_TRAIN_SITE ',
                                     'va.info.VQSR_POSITIVE_TRAIN_SITE = va.info.POSITIVE_TRAIN_SITE'])
-        .annotate_variants_expr('va.info = drop(va.info, culprit,NEGATIVE_TRAIN_SITE,POSITIVE_TRAIN_SITE, DS, END)')
+        .annotate_variants_expr('va.info = drop(va.info, culprit,NEGATIVE_TRAIN_SITE,POSITIVE_TRAIN_SITE, DS, END, HaplotypeScore)')
     )
 
 
