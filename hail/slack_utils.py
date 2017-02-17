3c74051dd76ded5b138195616af79f44108174b4
konradjk <konradjkarczewski@gmail.com>
2017-02-17 00:13:53 -0500

    Adding slack utils

---
+++ hail/slack_utils.py
@@ -0,0 +1,44 @@
+
+
+def get_slack_info():
+    import getpass
+    from slackclient import SlackClient
+    # import os
+    try:
+        from slack_creds import slack_token
+    except Exception, e:
+        return None
+
+    # slack_token = os.environ["SLACK_API_TOKEN"]
+    sc = SlackClient(slack_token)
+    user = getpass.getuser()
+    if user.startswith('konrad'): user = 'konradjk'
+    users = [x['name'] for x in sc.api_call("users.list")['members']]
+    default_channel = '#gnomad' if user not in users else '@' + user
+    return sc, default_channel
+
+
+def get_slack_channel_id(sc, channel):
+    channel_id = [x for x in sc.api_call('channels.list')['channels'] if x['name'] == channel]
+    return channel_id[0] if len(channel_id) else None
+
+
+def send_message(channel=None, message="Your job is done!"):
+    sc, default_channel = get_slack_info()
+
+    sc.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=message,
+        icon_emoji=':woohoo:',
+        parse='full'
+    )
+
+
+def send_snippet(channel=None, text='', filename='data.txt'):
+    sc, default_channel = get_slack_info()
+
+    get_channel = channel if channel is not None else default_channel
+    channel_id = get_slack_channel_id(sc, get_channel.lstrip('#'))
+
+    sc.api_call("files.upload", channel=channel_id, text=text, filename=filename)
