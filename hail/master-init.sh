0628d7e01091fa052fc971e93c8756a2968fb7b0
konradjk <konradjkarczewski@gmail.com>
2017-02-23 22:13:54 -0500

    Fixing up init scripts

--- hail/master-init.sh
+++ hail/master-init.sh
@@ -4,6 +4,7 @@
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
     gsutil cp gs://gnomad-public/tools/inits/gnomad-init.sh .
-    ./gnomad-init.sh &
+    chmod +x gnomad-init.sh
+    ./gnomad-init.sh > startup.log 2>&1 &
 fi
 
