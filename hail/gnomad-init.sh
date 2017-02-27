b0f8e2de26dcb1d61d065814d55e3f97e5fd0efb
konradjk <konradjkarczewski@gmail.com>
2017-02-27 13:41:04 -0500

    Init fixes

--- hail/gnomad-init.sh
+++ hail/gnomad-init.sh
@@ -2,7 +2,6 @@
 
 apt-get install -y ipython cmake gdebi-core libcurl4-openssl-dev libssl-dev libxml2-dev tmux
 
-easy_install pip
 pip install slackclient
 
 wget https://download2.rstudio.org/rstudio-server-1.0.136-amd64.deb
--- hail/master-init.sh
+++ hail/master-init.sh
@@ -3,8 +3,11 @@
 
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
+
+    easy_install pip
+    pip install decorator
+
     gsutil cp gs://gnomad-public/tools/inits/gnomad-init.sh .
     chmod +x gnomad-init.sh
     ./gnomad-init.sh > startup.log 2>&1 &
 fi
-
