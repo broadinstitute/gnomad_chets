b2860d236d43decc88ead05ba8633d47e1b205bc
konradjk <konradjkarczewski@gmail.com>
2017-03-25 14:55:08 -0400

    Adding some bashrc commands to init script for fun and profit

--- hail/gnomad-init.sh
+++ hail/gnomad-init.sh
@@ -14,4 +14,14 @@ git clone https://github.com/hail-is/hail.git
 cd hail
 ./gradlew shadowJar
 
-R --vanilla -e "install.packages(c('sparklyr', 'plyr', 'dplyr', 'ggplot2', 'magrittr', 'shiny', 'DT', 'tidyverse', 'broom', 'ggrepel', 'randomForest', 'ROCR', 'shinythemes', 'devtools'), repos='https://cran.rstudio.com')"
+cp -r /hail /hadoop_gcs_connector_metadata_cache/
+cat <<EOT >> /etc/bash.bashrc
+export SPARK_HOME=/usr/lib/spark
+export HAIL_HOME=/hadoop_gcs_connector_metadata_cache/hail
+export _JAVA_OPTIONS='-Xmx8096m'
+export PYTHONPATH=$SPARK_HOME/python:`ls $SPARK_HOME/python/lib/py4j-*-src.zip`:$HAIL_HOME/python:$PYTHONPATH
+export SPARK_CLASSPATH=$HAIL_HOME/build/libs/hail-all-spark.jar
+export HAIL_SPARK_PROPERTIES=spark.shuffle.compress=true,spark.kryoserializer.buffer.max=512m,spark.driver.memory=64G,spark.akka.frameSize=1024,spark.driver.maxResultSize=20G,spark.rdd.compress=true,spark.ui.port=54054,spark.executor.memory=200g
+EOT
+
+R --vanilla -e "install.packages(c('sparklyr', 'plyr', 'dplyr', 'ggplot2', 'magrittr', 'shiny', 'plotly', 'slackr', 'DT', 'tidyverse', 'broom', 'ggrepel', 'randomForest', 'ROCR', 'shinythemes', 'devtools'), repos='https://cran.rstudio.com')"
