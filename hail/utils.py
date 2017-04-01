d91f7ae7008fdcb28ac9f8fc49db5cf6dc68851e
laurent <lfran@broadinstitute.org>
2017-03-31 22:12:00 -0400

    auto-complete fix

--- hail/utils.py
+++ hail/utils.py
@@ -1472,7 +1472,7 @@ def annotate_subset_with_release(subset_vds, release_dict, root="va.info", dot_a
         annotations = filter_annotations_regex(annotations, ignore)
         a_annotations = filter_annotations_regex(a_annotations, ignore)
         g_annotations = filter_annotations_regex(g_annotations, ignore)
-        dot_annotations = filter_annotations_regex(dot_annotations_dict, ignore)
+        dot_annotations = filter_annotations_regex(dot_annotations, ignore)
 
     annotation_expr = ['%s = vds.find(x => isDefined(x)).%s.%s' % (release_dict['out_root'] + ann.name, ann_root, ann.name) for ann in annotations]
     annotation_expr.extend(['%s = orMissing(vds.exists(x => isDefined(x)  && isDefined(x.%s.%s)), range(v.nAltAlleles)'
