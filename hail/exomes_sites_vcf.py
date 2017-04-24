6c9cb1b60ce39613a3526936757d04b8cfb4f90f
konradjk <konradjkarczewski@gmail.com>
2017-04-23 20:05:26 -0400

    Final changes to exomes_sites_vcf.py for subset sites VCFs

--- hail/exomes_sites_vcf.py
+++ hail/exomes_sites_vcf.py
@@ -35,9 +35,6 @@ def preprocess_vds(vds, vqsr_vds, vds_pops=pops, release=True):
 
 
 def main(args):
-    out_vds_prefix = args.output
-    out_internal_vcf_prefix = args.output + '.internal'
-    out_external_vcf_prefix = args.output.replace('internal', 'vcf')
     if args.debug: logger.setLevel(logging.DEBUG)
     hc = HailContext()
 
@@ -52,24 +49,24 @@ def main(args):
     if not args.skip_preprocess_autosomes:
         (
             create_sites_vds_annotations(vds, pops, dbsnp_path=dbsnp_vcf)
-            .write(out_vds_prefix + ".pre.autosomes.vds")
+            .write(args.output + ".pre.autosomes.vds")
         )
 
     if not args.skip_preprocess_X:
         (
             create_sites_vds_annotations_X(vds, pops, dbsnp_path=dbsnp_vcf)
-            .write(out_vds_prefix + ".pre.X.vds")
+            .write(args.output + ".pre.X.vds")
         )
 
     if not args.skip_preprocess_Y:
         (
             create_sites_vds_annotations_Y(vds, pops, dbsnp_path=dbsnp_vcf)
-            .write(out_vds_prefix + ".pre.Y.vds")
+            .write(args.output + ".pre.Y.vds")
         )
 
     if not args.skip_merge:
         # Combine VDSes
-        vdses = [hc.read(args.output + ".pre.autosomes.vds"), hc.read(args.output + ".pre.X.vds"), hc.read(out_vds_prefix + ".pre.Y.vds")]
+        vdses = [hc.read(args.output + ".pre.autosomes.vds"), hc.read(args.output + ".pre.X.vds"), hc.read(args.output + ".pre.Y.vds")]
         vdses = merge_schemas(vdses)
         vds = vdses[0].union(vdses[1:])
         vds.write(args.output + '.pre.vds', overwrite=args.overwrite)
@@ -84,19 +81,19 @@ def main(args):
         vds = hc.read(args.output + ".pre.vep.vds")
         rf_vds = hc.read(rf_path)
         post_process_vds(vds, rf_vds, RF_SNV_CUTOFF, RF_INDEL_CUTOFF,
-                         'va.rf').write(out_vds_prefix + ".post.vds", overwrite=True)
+                         'va.rf').write(args.output + ".post.vds", overwrite=args.overwrite)
 
-        vds = hc.read(out_vds_prefix + ".post.vds")
-        sanity_check = run_sites_sanity_checks(vds, pops, return_string=True)
+        vds = hc.read(args.output + ".post.vds")
+        sanity_check = run_sites_sanity_checks(vds, pops)
         if args.slack_channel: send_snippet(args.slack_channel, sanity_check, 'sanity_%s.txt' % date_time)
 
     if not args.skip_write:
-        vds = hc.read(out_vds_prefix + ".post.vds").filter_variants_intervals(IntervalTree.read(exome_calling_intervals))
-        write_vcfs(vds, '', out_internal_vcf_prefix, out_external_vcf_prefix, RF_SNV_CUTOFF, RF_INDEL_CUTOFF, append_to_header=additional_vcf_header)
-        write_public_vds(vds, out_external_vcf_prefix.replace('vcf', 'vds') + ".vds")
+        vds = hc.read(args.output + ".post.vds").filter_variants_intervals(IntervalTree.read(exome_calling_intervals))
+        write_vcfs(vds, '', args.output, False, RF_SNV_CUTOFF, RF_INDEL_CUTOFF, append_to_header=additional_vcf_header)
+        write_public_vds(vds, args.output + ".vds", overwrite=args.overwrite)
 
     if not args.skip_pre_calculate_metrics:
-        vds = hc.read(out_external_vcf_prefix.replace('vcf', 'vds') + ".autosomes.vds")
+        vds = hc.read(args.output + ".vds")
         pre_calculate_metrics(vds, "exome_precalculated_metrics.txt")
         send_snippet('#exac_browser', open('exome_precalculated_metrics.txt').read())
 
