da31e030a12425b47a91aa8425f7bf035ff48d9e
laurent <lfran@broadinstitute.org>
2017-05-30 16:35:54 -0400

    - Updated exome hardcall files to point to newer files (containing all samples, with new annotations) - Added more useful resources

--- hail/resources.py
+++ hail/resources.py
@@ -4,7 +4,7 @@ omni_path = "%s/1000G_omni2.5.b37.vds" % truth_dir
 mills_path = "%s/Mills_and_1000G_gold_standard.indels.b37.vds" % truth_dir
 hapmap_path = "%s/hapmap_3.3.b37.vds" % truth_dir
 dbsnp_vcf = "%s/vcf/All_20160601.vcf.bgz" % truth_dir
-kgp_high_conf_snvs = "%s/vcf/1000G_phase1.snps.high_confidence.b37.vcf.bgz" % truth_dir
+kgp_high_conf_snvs = "%s/vcf/1000G_phase1.snps.high_confidence.b37.vds" % truth_dir
 NA12878_path = "%s/NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid-10X_CHROM1-X_v3.3_highconf.vds" % truth_dir
 NA12878_high_conf_regions_path = "%s/NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid-10X_CHROM1-X_v3.3_highconf.bed" % truth_dir
 NA12878_high_conf_exome_regions_path = "gs://exac2/intervals/union13callableMQonlymerged_addcert_nouncert_excludesimplerep_excludesegdups_excludedecoy_excludeRepSeqSTRs_noCNVs_v2.18_2mindatasets_5minYesNoRatio.bed"
@@ -38,8 +38,8 @@ vep_config = "/vep/vep-gcloud.properties"
 # Full VDSs
 full_exac_v1_vds = 'gs://gnomad-exomes-raw/exacv1/exac.all.vds'
 full_exome_vds = 'gs://gnomad-exomes-raw/full/gnomad.exomes.all.vds'
-full_exome_hardcalls_vds = 'gs://gnomad-exomes-raw/hardcalls/gnomad.exomes.raw.hardcalls.qc.vds'
-full_exome_hardcalls_split_vds = 'gs://gnomad-exomes-raw/hardcalls/gnomad.exomes.raw.hardcalls.splitmulti.qc.vds'
+full_exome_hardcalls_vds = 'gs://gnomad-exomes-raw/hardcalls/gnomad.exomes.raw_hardcalls.vds'
+full_exome_hardcalls_split_vds = 'gs://gnomad-exomes-raw/hardcalls/gnomad.exomes.raw_hardcalls.split.vds'
 full_genome_vds = 'gs://gnomad-genomes-raw/full/gnomad.genomes.all.vds'
 full_genome_hardcalls_vds = "gs://gnomad-genomes-raw/hardcalls/gnomad.raw_hardcalls.vds"
 full_genome_hardcalls_split_vds = "gs://gnomad-genomes-raw/hardcalls/gnomad.raw_hardcalls.split.vds"
@@ -56,12 +56,15 @@ final_exac_sites_vds = 'gs://gnomad-exomes-raw/exacv1/exac.sites.vds'
 final_exome_autosomes = 'gs://gnomad-public/release-170228/gnomad.exomes.r2.0.1.sites.autosomes.vds'
 final_genome_autosomes = 'gs://gnomad-public/release-170228/gnomad.genomes.r2.0.1.sites.autosomes.vds'
 final_exome_vds = 'gs://gnomad-public/release-170228/gnomad.exomes.r2.0.1.sites.vds'
+final_exome_split_vds = 'gs://gnomad-exomes/sites/vds/gnomad.exomes.r2.0.1.sites.split.vds'
 final_genome_vds = 'gs://gnomad-public/release-170228/gnomad.genomes.r2.0.1.sites.vds'
+final_genome_split_vds = 'gs://gnomad-genomes/sites/vds/gnomad.genomes.r2.0.1.sites.split.vds'
 
 #Meta data
 genomes_meta = "gs://gnomad-genomes-raw/gnomad.final.all_meta.txt.bgz"
 genomes_fam = "gs://gnomad-genomes-raw/gnomad.final.goodTrios.fam"
 exomes_meta = 'gs://gnomad-exomes-raw/super_meta_april_01_2017.txt.bgz'
+exomes_fam = "gs://gnomad-exomes/variantqc/gnomad_exomes.qctrios.fam"
 
 #PCA
 gnomad_pca = "gs://gnomad-genomes/sampleqc/gnomad.pca.vds"
