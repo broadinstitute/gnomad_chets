bc15746aaafe7cc05a934dc37a188683f73d27a8
konradjk <konradjkarczewski@gmail.com>
2017-07-24 10:23:31 -0400

    Harmonizing inits

--- hail/inits/master-init.sh
+++ hail/inits/master-init.sh
@@ -4,9 +4,20 @@
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
 
-    easy_install pip
-    pip install decorator
+    export PATH=/home/anaconda2/bin:${PATH}
+    export SPARK_HOME=/usr/lib/spark
+    export HAIL_HOME=/home/hail
 
+cat <<EOT >> /etc/bash.bashrc
+export PATH=/home/anaconda2/bin:${PATH}
+export SPARK_HOME=${SPARK_HOME}
+export HAIL_HOME=${HAIL_HOME}
+export _JAVA_OPTIONS='-Xmx8096m'
+export PYTHONPATH=${SPARK_HOME}/python:$(ls ${SPARK_HOME}/python/lib/py4j-*-src.zip):$(ls ${HAIL_HOME}/pyhail-*zip)
+export SPARK_CLASSPATH=${HAIL_HOME}/${HAIL_JAR}
+EOT
+
+    pip install slackclient sklearn
     gsutil cp gs://gnomad-public/tools/inits/gnomad-init.sh .
     chmod +x gnomad-init.sh
     ./gnomad-init.sh > startup.log 2>&1 &
