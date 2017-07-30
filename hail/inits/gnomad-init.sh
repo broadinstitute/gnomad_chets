62ce6df12c85d727c6b5c4aa887e741266d72d36
konradjk <konradjkarczewski@gmail.com>
2017-07-30 10:43:32 -0400

    Revert "Harmonizing inits"
    This reverts commit bc15746aaafe7cc05a934dc37a188683f73d27a8.

--- hail/inits/gnomad-init.sh
+++ hail/inits/gnomad-init.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
 # iPython and hail master as quickly as possible
-apt-get install -y tmux
+apt-get install -y ipython tmux
+
+pip install --upgrade requests  # Slackclient has the wrong dependency
+pip install slackclient pandas scipy sklearn statsmodels
+
+export SPARK_HOME=/usr/lib/spark
+export HAIL_HOME=/hadoop_gcs_connector_metadata_cache/hail
+export HAIL_HASH=$(gsutil cat gs://hail-common/latest-hash.txt)
+export HAIL_JAR=hail-hail-is-master-all-spark2.0.2-${HAIL_HASH}.jar
+export HAIL_PYTHON_ZIP=pyhail-hail-is-master-${HAIL_HASH}.zip
+mkdir $HAIL_HOME
+gsutil cp gs://hail-common/${HAIL_JAR} gs://hail-common/${HAIL_PYTHON_ZIP} $HAIL_HOME
+
+# Prepare bashrc and redownload script
+cat <<EOT >> /etc/bash.bashrc
+export SPARK_HOME=${SPARK_HOME}
+export HAIL_HOME=${HAIL_HOME}
+export HAIL_HASH=${HAIL_HASH}
+export HAIL_JAR=${HAIL_JAR}
+export HAIL_PYTHON_ZIP=${HAIL_PYTHON_ZIP}
+export _JAVA_OPTIONS='-Xmx8096m'
+export PYTHONPATH=${SPARK_HOME}/python:$(ls ${SPARK_HOME}/python/lib/py4j-*-src.zip):${HAIL_HOME}/${HAIL_PYTHON_ZIP}
+export SPARK_CLASSPATH=${HAIL_HOME}/${HAIL_JAR}
+EOT
+cat <<EOT >> /redownload_hail.sh
+mkdir -p $HAIL_HOME
+gsutil cp gs://hail-common/${HAIL_JAR} gs://hail-common/${HAIL_PYTHON_ZIP} $HAIL_HOME
+EOT
+chmod +x /redownload_hail.sh
 
 # R stuff from here
 apt-get install -y gdebi-core libcurl4-openssl-dev libssl-dev libxml2-dev
@@ -16,6 +44,14 @@ R --vanilla -e "install.packages(c('sparklyr', 'dplyr'), repos='https://cran.rst
 R --vanilla -e "install.packages(c('magrittr', 'ggplot2', 'slackr', 'ggrepel'), repos='https://cran.rstudio.com')"
 R --vanilla -e "install.packages(c('plyr', 'shiny', 'plotly'), repos='https://cran.rstudio.com')"
 R --vanilla -e "install.packages(c('DT', 'tidyverse', 'broom', 'randomForest', 'ROCR', 'shinythemes', 'devtools'), repos='https://cran.rstudio.com')"
+
+# Building Hail. Why not.
+apt-get install -y cmake
+git clone https://github.com/hail-is/hail.git
+cd hail
+./gradlew shadowJar
+mkdir /hadoop_gcs_connector_metadata_cache/hail_build/
+cp -r * /hadoop_gcs_connector_metadata_cache/hail_build/
 
 # curl http://www.freefontspro.com/d/14454/arial.zip > /home/anaconda2/lib/python2.7/site-packages/matplotlib/mpl-data/fonts/ttf/arial.zip
 # unzip /home/anaconda2/lib/python2.7/site-packages/matplotlib/mpl-data/fonts/ttf/arial.zip
--- hail/inits/master-init.sh
+++ hail/inits/master-init.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-export HAIL_HOME=/home/hail
-export SPARK_HOME=/usr/lib/spark
-
-HASH=$(gsutil cat gs://hail-common/latest-hash.txt)
-JAR=hail-hail-is-master-all-spark2.0.2-${HASH}.jar
-ZIP=pyhail-hail-is-master-${HASH}.zip
-
-mkdir -p ${HAIL_HOME}
-gsutil cp gs://hail-common/${JAR} gs://hail-common/${ZIP} ${HAIL_HOME}
 
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
 
-cat <<EOT >> /etc/bash.bashrc
-export HAIL_HOME=/home/hail
-export SPARK_HOME=/usr/lib/spark
-export PATH=/home/anaconda2/bin:${PATH}
-export _JAVA_OPTIONS='-Xmx8096m'
-export PYTHONPATH=${SPARK_HOME}/python:$(ls ${SPARK_HOME}/python/lib/py4j-*-src.zip):$(ls ${HAIL_HOME}/pyhail-*zip)
-export SPARK_CLASSPATH=$(ls ${HAIL_HOME}/hail-*jar)
-EOT
-
+    easy_install pip
     pip install slackclient sklearn google_compute_engine
     gsutil cp gs://gnomad-public/tools/inits/gnomad-init.sh .
     chmod +x gnomad-init.sh
