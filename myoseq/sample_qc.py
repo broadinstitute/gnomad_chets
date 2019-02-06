5fe978d5c7eb788867166b776e89a4ab82603368
laurent <lfran@broadinstitute.org>
2019-02-06 16:50:08 -0500

    Refactored to put as much as possible into methods that should eventually move to `gnomad_hail` * Enhanced dups filtering * Added related filtering

--- myoseq/sample_qc.py
+++ myoseq/sample_qc.py
@@ -4,7 +4,7 @@ import hail as hl
 import numpy as np
 import hdbscan
 
-
+# TODO: Move to gnomad_hail common repo
 def assign_platform_pcs(platform_pc_table: hl.Table, pc_scores_ann: str = 'scores') -> hl.Table:
 
     # Read and format data for clustering
@@ -32,6 +32,7 @@ def compute_callrate_mt(mt: hl.MatrixTable,  intervals: hl.Table) -> hl.MatrixTa
     callrate_mt = callrate_mt.group_rows_by(callrate_mt.interval).aggregate(callrate=hl.agg.fraction(hl.is_defined(callrate_mt.GT)))
     return  callrate_mt
 
+
 def run_platform_pca(callrate_mt: hl.MatrixTable) -> hl.Table:
     callrate_mt = callrate_mt.annotate_entries(callrate=hl.int(callrate_mt.callrate > 0.25))
     # Center until Hail's PCA does it for you
@@ -39,12 +40,12 @@ def run_platform_pca(callrate_mt: hl.MatrixTable) -> hl.Table:
     callrate_mt = callrate_mt.annotate_entries(callrate=callrate_mt.callrate - callrate_mt.mean_callrate)
     eigenvalues, scores, _ = hl.pca(callrate_mt.callrate, compute_loadings=False)
     logger.info('Eigenvalues: {}'.format(eigenvalues))
-    # [731282566.2824697, 78687228.90071851, 43837650.51729764, 33969298.61827205, 26308703.539534636, 21102437.512725923, 16949828.555817757, 12994894.187041137, 8372332.274295175, 8128326.814388647]
+
     return scores
 
 
 def impute_sex(
-        x_mt: hl.MatrixTable,
+        x_mt: hl.MatrixTable, # TODO: This feels somewhat unsatisfying to provide two MTs. Maybe just reapply the QC MT filters to both (minus callrate for Y)?
         y_mt: hl.MatrixTable,
         platform_ht: hl.Table,
         male_min_f_stat: float,
@@ -95,30 +96,131 @@ def impute_sex(
     return(sex_ht)
 
 
+def get_related_samples_to_drop(relatedness_ht: hl.Table, min_filtering_kinship: float, rank_func: Callable, rank_func_args) -> hl.Table:
+    """
+    Use the maximal independence function in Hail to intelligently prune clusters of related individuals, removing
+    less desirable samples while maximizing the number of unrelated individuals kept in the sample set
+
+    :param Table rank_table: Table with ranking annotations across exomes and genomes, computed via make_rank_file()
+    :param Table relatedness_ht: Table with kinship coefficient annotations computed via pc_relate()
+    :return: Table containing sample IDs ('s') to be pruned from the combined exome and genome sample set
+    :rtype: Table
+    """
+    # Define maximal independent set, using rank list
+    related_pairs = relatedness_ht.filter(relatedness_ht.kin > min_filtering_kinship).key_by().select('i', 'j')
+    n_related_samples = related_pairs.annotate(ij=[related_pairs.i, related_pairs.j]).explode('ij').key_by('ij').distinct().count()
+    logger.info('{} samples with at least 2nd-degree relatedness found in callset'.format(n_related_samples))
+
+    related_pairs, tie_breaker =  rank_func(relatedness_ht, *rank_func_args)
+
+    related_samples_to_drop_ranked = hl.maximal_independent_set(related_pairs.i, related_pairs.j,
+                                                                keep=False, tie_breaker=tie_breaker)
+    return related_samples_to_drop_ranked.select(**related_samples_to_drop_ranked.node.id).key_by('s')
+
+
+def filter_dups(relatedness_ht: hl.Table, dup_ranking_func: Callable, dup_ranking_func_args: List[Any]):
+    logger.info("Getting duplicate samples")
+    dups = get_duplicated_samples(relatedness_ht)
+    logger.info(f"Found {len(dups)} duplicate sets.")
+    dups_ht = hl.Table.parallelize([hl.struct(dup_set=i, dups=dups[i]) for i in range(0, len(dups))])
+    dups_ht = dups_ht.explode(dups_ht.dups, name='s')
+    dups_ht = dups_ht.key_by(**dups_ht.s)
+    dups_ht = dup_ranking_func(dups_ht, *dup_ranking_func_args)
+    dups_cols = hl.bind(
+        lambda x: hl.struct(
+            kept=x[0],
+            filtered=x[1:]
+        ),
+        hl.sorted(hl.agg.collect(hl.tuple([dups_ht.s, dups_ht.rank])), key=lambda x: x[1]).map(lambda x: x[0])
+    )
+    dups_ht = dups_ht.group_by(dups_ht.dup_set).aggregate(
+        **dups_cols
+    )
+    dups_ht = dups_ht.key_by(**{f'{x}_kept': expr for x, expr in dups_ht.kept.items()}).drop('kept')
+    return dups_ht
+
+
+def get_qc_mt(mt: hl.MatrixTable, min_af: float = 0.001, min_callrate: float = 0.99, ld_r2: float = 0.1) -> hl.MatrixTable:
+    qc_mt = mt.filter_rows((hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1]) &
+                           (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > min_af) &
+                           (hl.agg.fraction(hl.is_defined(mt.GT)) > min_callrate)).persist()
+    pruned_ht = hl.ld_prune(qc_mt.GT, r2=ld_r2)
+    qc_mt = qc_mt.filter_rows(hl.is_defined(pruned_ht[qc_mt.row_key]))
+    return qc_mt.annotate_cols(callrate=hl.agg.fraction(hl.is_defined(qc_mt.GT)))
+
+
+def get_ped(relatedness_ht: hl.Table, dups_ht: hl.Table, sex_ht: hl.Table) -> hl.Pedigree:
+    relatedness_ht = relatedness_ht.key_by(  # Needed for familial inference at this point -- should be generalized
+        i=relatedness_ht.i.s,
+        j=relatedness_ht.j.s
+    )
+    dups_to_remove = dups_ht.aggregate(hl.agg.explode(lambda x: hl.agg.collect_as_set(x.s), dups_ht.filtered))
+    logger.info(f"Removing {len(dups_to_remove)} duplicates from family creation.")
+    sex = {row.s: row.is_female for row in sex_ht.to_pandas().itertuples()}
+    ped = infer_families(relatedness_ht, sex, dups_to_remove)
+    logger.info(f"Found {len(ped.complete_trios())} complete trios.")
+    return ped
+
+
+# MYOSEQ-specific methods
+
+def rank_samples(related_pairs: hl.Table, meta_ht: hl.Table, qc_ht: hl.Table, fam_ht: hl.Table) -> Tuple[hl.Table, Callable]: # TODO: This should be loaded dynamically from a separate class to be project-customizable
+    # Load families and identify parents from cases as they will be thrown away anyways
+    fam_ht = fam_ht.transmute(trio=[
+        hl.struct(s=fam_ht.id, is_parent=False),
+        hl.struct(s=fam_ht.pat_id, is_parent=True),
+        hl.struct(s=fam_ht.mat_id, is_parent=True)
+    ])
+    fam_ht = fam_ht.explode(fam_ht.trio)
+    fam_ht = fam_ht.key_by(s=fam_ht.trio.s)
+    case_parents = fam_ht.filter(meta_ht[fam_ht.key].is_case & fam_ht.trio.is_parent)
+
+    def annotate_related_pairs(related_pairs: hl.Table, index_col: str) -> hl.Table:
+        related_pairs = related_pairs.key_by(**related_pairs[index_col])
+        related_pairs = related_pairs.filter(hl.is_missing(case_parents))
+        return related_pairs.annotate(
+            **{
+                index_col: related_pairs[index_col].annotate(
+                    case_rank=hl.or_else(hl.int(meta_ht[related_pairs.key].is_case), -1),
+                    dp_mean=hl.or_else(qc_ht[related_pairs.key].sample_qc.dp_stats.mean, -1.0)
+                )
+            }
+        ).key_by()
+
+    related_pairs = annotate_related_pairs(related_pairs, "i")
+    related_pairs = annotate_related_pairs(related_pairs, "j")
+
+    def tie_breaker(l, r):
+        return (
+            hl.case()
+            .when(l.case_rank != r.case_rank, r.case_rank - l.case_rank) # smaller is better
+            .default(l.dp_mean - r.dp_mean) # larger is better
+        )
+
+    return related_pairs, tie_breaker
+
+
+def rank_dup_samples(dups_ht: hl.Table, qc_ht: hl.Table):
+    return dups_ht.annotate(rank=-1*qc_ht[dups_ht.key].sample_qc.dp_stats.mean)
+
+
 def main(args):
 
     output_prefix = args.output_dir.rstrip("/") + "/" + splitext(basename(args.input_mt))[0]
 
-    mt = hl.read_matrix_table(args.input_mt)
-
     if args.compute_qc_mt:
         logger.info("Filtering to bi-allelic, high-callrate, common SNPs for sample QC...")
-        qc_mt = mt.filter_rows((hl.len(mt.alleles) == 2) & hl.is_snp(mt.alleles[0], mt.alleles[1]) &
-                            (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001) &
-                            (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99)).persist()
-        pruned_ht = hl.ld_prune(qc_mt.GT, r2=0.1)
-        qc_mt = qc_mt.filter_rows(hl.is_defined(pruned_ht[qc_mt.row_key]))
-        qc_mt = qc_mt.annotate_cols(callrate=hl.agg.fraction(hl.is_defined(qc_mt.GT)))
+        qc_mt = get_qc_mt(hl.read_matrix_table(args.input_mt))
         qc_mt.write(f'{output_prefix}.qc.mt', overwrite=args.overwrite)
 
     if args.compute_qc_metrics:
-        qc_ht = hl.sample_qc(mt).cols()
+        qc_ht = hl.sample_qc(hl.read_matrix_table(args.input_mt)).cols()
         qc_ht.write(f'{output_prefix}.sample_qc.ht', overwrite=args.overwrite)
 
     if args.compute_callrate_mt:
         logger.info('Preparing data for platform PCA...')
         intervals = hl.import_locus_intervals(evaluation_intervals_path)
-        callrate_mt = compute_callrate_mt(mt, intervals)
+        callrate_mt = compute_callrate_mt(hl.read_matrix_table(args.input_mt), intervals)
         callrate_mt.write(f'{output_prefix}.callrate.mt', args.overwrite)
 
     if args.run_platform_pca:
@@ -132,9 +234,15 @@ def main(args):
         platform_ht.write(f'{output_prefix}.platform_pca_results.ht', overwrite=args.overwrite)
 
     if args.impute_sex:
-        platform_ht = hl.read_table(f'{output_prefix}.platform_pca_results.ht')
-        qc_mt = hl.read_matrix_table(f'{output_prefix}.qc.mt')
-        sex_ht =  impute_sex(qc_mt, mt, platform_ht, args.male_threshold, args.female_threshold, args.min_male_y_sites_called, args.y_call_rate_stdev)
+        sex_ht = impute_sex(
+            hl.read_matrix_table(f'{output_prefix}.qc.mt'),
+            hl.read_matrix_table(args.input_mt),
+            hl.read_table(f'{output_prefix}.platform_pca_results.ht'),
+            args.male_threshold,
+            args.female_threshold,
+            args.min_male_y_sites_called,
+            args.y_call_rate_stdev
+        )
         sex_ht.write(f'{output_prefix}.sex.ht', overwrite=args.overwrite)
 
     if args.run_pc_relate:
@@ -147,35 +255,37 @@ def main(args):
         scores = hl.read_table(f'{output_prefix}.pruned.pca_scores.ht')
         # NOTE: This needs SSDs on your workers (for the temp files) and no pre-emptibles while the BlockMatrix writes
         relatedness_ht = hl.pc_relate(qc_mt.GT, min_individual_maf=0.05, scores_expr=scores[qc_mt.col_key].scores,
-                                      block_size=4096, min_kinship=args.min_kinship, statistics='kin2')
+                                      block_size=4096, min_kinship=args.min_emission_kinship, statistics='kin2')
         relatedness_ht.write(f'{output_prefix}.relatedness.ht', args.overwrite)
 
-    if args.filter_related_samples:
-        pass
-
-
-    if args.filter_dups: # Note, this needs a prioritization scheme
-        relatedness_ht = hl.read_table(f'{output_prefix}.relatedness.ht')
-        logger.info("Getting duplicate samples")
-        dups = get_duplicated_samples(relatedness_ht)
-        dups_to_keep = [hl.struct(kept=list(d)[0], filtered=list(d)[1:]) for d in dups]  # Dict of kept -> filtered (random selection)
-        dups_ht = hl.Table.parallelize(dups_to_keep, key='kept')
+    if args.filter_dups:
+        dups_ht = filter_dups(
+            hl.read_table(f'{output_prefix}.relatedness.ht'),
+            rank_dup_samples,
+            [hl.read_table(f'{output_prefix}.sample_qc.ht')]
+        )
         dups_ht.write(f'{output_prefix}.dups.ht', overwrite=args.overwrite)
 
     if args.infer_families:
-        relatedness_ht = hl.read_table(f'{output_prefix}.relatedness.ht')
-        relatedness_ht = relatedness_ht.key_by( # Needed for familial inference at this point
-            i=relatedness_ht.i.s,
-            j=relatedness_ht.j.s
+        ped = get_ped(
+            hl.read_table(f'{output_prefix}.relatedness.ht'),
+            hl.read_table(f'{output_prefix}.dups.ht'),
+            hl.read_table(f'{output_prefix}.sex.ht')
         )
-        dups_ht = hl.read_table(f'{output_prefix}.dups.ht')
-        dups_to_remove = dups_ht.aggregate(hl.agg.explode(lambda x: hl.agg.collect_as_set(x.s), dups_ht.filtered))
-        logger.info(f"Found {len(dups)} duplicate sets; marking {len(dups_to_remove)} duplicates")
-        x_ht = hl.read_table(f'{output_prefix}.sex.ht')
-        sex = {row.s: row.is_female for row in x_ht.to_pandas().itertuples()}
-        raw_ped = infer_families(relatedness_ht, sex, dups_to_remove)
-        logger.info(f"Found {len(raw_ped.complete_trios())} complete trios.")
-        raw_ped.write(f"{output_prefix}.ped")
+        ped.write(f"{output_prefix}.ped")
+
+    if args.filter_related_samples:
+        related_samples_to_drop = get_related_samples_to_drop(
+            hl.read_table(f'{output_prefix}.relatedness.ht'),
+            args.min_filtering_kinship,
+            rank_samples,
+            [
+                hl.read_table(args.meta),
+                hl.read_table(f'{output_prefix}.sample_qc.ht'),
+                hl.import_fam(f"{output_prefix}.ped", delimiter="\t")
+            ]
+        )
+        related_samples_to_drop.write(f'{output_prefix}.related_samples_to_drop.ht')
 
     if args.run_pca:
         pass
@@ -186,7 +296,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--slack_channel', help='Slack channel to post results and notifications to.')
-    parser.add_argument('--input_mt', help='Input MT', default='gs://gnomad/projects/compound_hets/myoseq/MacArthur_LGMD_Callset_Jan2019.vcf.split.mt')
+    parser.add_argument('--input_mt', help='Input MT', default='gs://gnomad/projects/compound_hets/myoseq/MacArthur_LGMD_Callset_Jan2019.mt')
+    parser.add_argument('--meta', help='Location of the metadata HT', default="gs://gnomad/projects/compound_hets/myoseq/LGMD_VCF_Detail_Metrics_V2_01302019.ht")
     parser.add_argument('--output_dir', help='Directory to output files to.', default='gs://gnomad/projects/compound_hets/myoseq/sample_qc')
     parser.add_argument('--overwrite', help='Overwrite all data from this subset (default: False)', action='store_true')
 
@@ -206,9 +317,11 @@ if __name__ == '__main__':
     sex_imputations.add_argument('--y_call_rate_stdev', help='Number of standard deviation from the mean of the platform Y call rate to remove a sample based on its Y call rate', default=4, type=int)
 
     relatedness = parser.add_argument_group("Relatedness")
-    relatedness.add_argument('--run_pc_relate', help='Runs PC-relate on all samples', action='store_true')
-    relatedness.add_argument('--min_kinship', help='Minimum kinship threshold for emitting a pair of samples in PC relate and filtering related individuals.', default=0.05, type=float)
-    relatedness.add_argument('--filter_related_samples', help='Filter related samples (based on the pairs present from the --run_pc_relate, so using the --min_kinship value for that run)', action='store_true')
+    relatedness.add_argument('--run_pc_relate', help='Runs PC-relate on all samples. NOTE: This needs SSDs on your workers (for the temp files) and no pre-emptibles while the BlockMatrix writes', action='store_true')
+    relatedness.add_argument('--min_emission_kinship', help='Minimum kinship threshold for emitting a pair of samples in PC relate and filtering related individuals.', default=0.05, type=float)
+    relatedness.add_argument('--min_filtering_kinship', help='Minimum kinship threshold for filtering a pair of samples in PC relate and filtering related individuals. (Default = 0.08838835; 2nd degree relatives)', default= 0.08838835, type=float)
+    relatedness.add_argument('--filter_related_samples', help='Filter related samples (based on the pairs present from the --run_pc_relate and using the --min_filtering_kinship value for that run)', action='store_true')
+
     relatedness.add_argument('--filter_dups', help='Filter duplicated samples', action='store_true')
     relatedness.add_argument('--infer_families', help='Extracts duplicate samples and infers families samples based on PC-relate results', action='store_true')
 
