47acb45f213dabd4d46f619d1a0b7ddfbc1b670d
laurent <lfran@broadinstitute.org>
2020-06-08 10:06:45 -0400

    creeate_per_gene_matrix now working and producing exactly the same counts on chrom20 - Reviewed logic for computing the per-gene counts both from VP_MT and MT

--- create_per_gene_matrix.py
+++ create_per_gene_matrix.py
@@ -1,21 +1,21 @@
 from gnomad_qc.v2.resources import get_gnomad_meta, get_gnomad_data, annotations_ht_path
 import hail as hl
-from typing  import List, Union
+from typing import List, Union
 import argparse
 from resources import *
 
 CHET_THRESHOLD = 0.5
 SAME_HAP_THRESHOLD = 0.5
 
-CSQ_CODES = {
-        'lof': 0,
-        'damaging_missense': 1,
-        'missense_variant': 2,
-        'synonymous_variant': 3
-    }
+CSQ_CODES = [
+    'lof',
+    'damaging_missense',
+    'missense_variant',
+    'synonymous_variant'
+]
 
 
-def filter_to_chr20(tables: List[Union[hl.Table, hl.MatrixTable]]) ->  List[Union[hl.Table, hl.MatrixTable]]:
+def filter_to_chr20(tables: List[Union[hl.Table, hl.MatrixTable]]) -> List[Union[hl.Table, hl.MatrixTable]]:
     return [hl.filter_intervals(t, [hl.parse_locus_interval('20')]) for t in tables]
 
 
@@ -34,18 +34,19 @@ def get_group_to_counts_expr(k: hl.expr.StructExpression, counts: hl.expr.DictEx
 
 
 def get_worst_gene_csq_code_expr(vep_expr: hl.expr.StructExpression) -> hl.expr.DictExpression:
-
-    worst_gene_csq_expr = vep_expr.transcript_consequences.map(
+    worst_gene_csq_expr = vep_expr.transcript_consequences.filter(
+        lambda tc: tc.biotype == 'protein_coding'
+    ).map(
         lambda ts: ts.select(
             'gene_id',
             'gene_symbol',
             csq=(
                 hl.case(missing_false=True)
-                .when(ts.lof == 'HC', CSQ_CODES['lof'])
-                .when(ts.polyphen_prediction == 'probably_damaging', CSQ_CODES['damaging_missense'])
-                .when(ts.consequence_terms.any(lambda x: x == 'missense_variant'), CSQ_CODES['missense_variant'])
-                .when(ts.consequence_terms.all(lambda x: x == 'synonymous_variant'), CSQ_CODES['synonymous_variant'])
-                .or_missing()
+                    .when(ts.lof == 'HC', CSQ_CODES.index('lof'))
+                    .when(ts.polyphen_prediction == 'probably_damaging', CSQ_CODES.index('damaging_missense'))
+                    .when(ts.consequence_terms.any(lambda x: x == 'missense_variant'), CSQ_CODES.index('missense_variant'))
+                    .when(ts.consequence_terms.all(lambda x: x == 'synonymous_variant'), CSQ_CODES.index('synonymous_variant'))
+                    .or_missing()
             )
         )
     )
@@ -60,9 +61,8 @@ def get_worst_gene_csq_code_expr(vep_expr: hl.expr.StructExpression) -> hl.expr.
 
 
 def compute_from_vp_mt(chr20: bool, overwrite: bool):
-
     def get_popmax_af(freq_dict):
-        pops_to_exclude = hl.literal({'asj', 'fin', 'oth', 'all'}) # TODO:  decide what to include/exclude here
+        pops_to_exclude = hl.literal({'asj', 'fin', 'oth', 'all'})  # TODO:  decide what to include/exclude here
         # pops_to_exclude = hl.literal({'oth'})
         return hl.max(
             freq_dict.keys().filter(
@@ -73,7 +73,7 @@ def compute_from_vp_mt(chr20: bool, overwrite: bool):
         )
 
     meta = get_gnomad_meta('exomes')
-    vp_mt = hl.read_matrix_table(full_mt_path('exomes'))  # TODO check that this doesn't contain high quality non-releasable samples
+    vp_mt = hl.read_matrix_table(full_mt_path('exomes'))
     vp_mt = vp_mt.filter_cols(meta[vp_mt.col_key].release)
     ann_ht = hl.read_table(vp_ann_ht_path('exomes'))
     phase_ht = hl.read_table(phased_vp_count_ht_path('exomes'))
@@ -92,7 +92,7 @@ def compute_from_vp_mt(chr20: bool, overwrite: bool):
             lambda k: vep2_expr.contains(k)
         ).map(
             lambda k: vep1_expr[k].annotate(
-                csq=hl.sorted([vep1_expr[k].csq, vep2_expr[k].csq])
+                csq=hl.max(vep1_expr[k].csq, vep2_expr[k].csq)
             )
         )
     )
@@ -125,104 +125,73 @@ def compute_from_vp_mt(chr20: bool, overwrite: bool):
 
     vp_mt.describe()
 
+    def get_grouped_phase_agg():
+        return hl.agg.group_by(
+            hl.case()
+                .when(vp_mt.phase_info[vp_mt.pop].em.adj.p_chet > CHET_THRESHOLD, 1)
+                .when(vp_mt.phase_info[vp_mt.pop].em.adj.p_chet < SAME_HAP_THRESHOLD, 2)
+                .default(3)
+            ,
+            hl.agg.min(vp_mt.csq)
+        )
+
     vp_mt = vp_mt.group_rows_by(
         'gene_id',
         'gene_symbol'
     ).aggregate(
-        counts=hl.agg.filter(
+        af_gt_0_001=hl.agg.filter(
             vp_mt.x,
-            hl.agg.group_by(
-                hl.struct(
-                    snv=vp_mt.snv1 & vp_mt.snv2,
-                    af_gt_0_001=(vp_mt.popmax > 0.001),
-                    csq=vp_mt.csq
-                ),
-                hl.struct(
-                    n=hl.agg.count(),
-                    max_p_chet=hl.agg.max(vp_mt.phase_info[vp_mt.pop].em.adj.p_chet)
-                )
-            )
+            get_grouped_phase_agg()
+        ),
+        af_le_0_001=hl.agg.filter(
+            (vp_mt.popmax <= 0.001) & vp_mt.x,
+            get_grouped_phase_agg()
         )
     )
 
-    # if args.impact_is_at_least:
-    #     vp_mt = vp_mt.annotate_entries(
-    #         counts=vp_mt.counts.keys().map(
-    #             lambda k: (
-    #                 k,
-    #                 get_group_to_counts_expr(k, vp_mt.counts).fold(
-    #                     lambda i, j: hl.struct(
-    #                         n=i.n + j.n,
-    #                         max_p_chet=hl.if_else(
-    #                             i.max_p_chet > j.max_p_chet,
-    #                             i.max_p_chet,
-    #                             j.max_p_chet
-    #                         )
-    #                     ),
-    #                     hl.struct(n=0, max_p_chet=-1.0)
-    #                 )
-    #             )
-    #         )
-    #     )
+    vp_mt = vp_mt.checkpoint('gs://gnomad-tmp/test.mt', overwrite=True)
 
     gene_ht = vp_mt.annotate_rows(
-        row_counts=hl.array(
-            hl.agg.explode(
-                lambda x:
-                hl.agg.filter(
-                    x[1].n > 0,
-                    hl.agg.group_by(
-                        x[0].annotate(
-                            pop=vp_mt.pop,
-                            csq1=hl.literal(list(CSQ_CODES.keys()))[x[0].csq[0]],
-                            csq2=hl.literal(list(CSQ_CODES.keys()))[x[0].csq[1]],
-                        ),
-                        hl.struct(
-                            total=hl.agg.count(),
-                            phased_c_het=hl.agg.count_where(x[1].max_p_chet > CHET_THRESHOLD),
-                            phased_same_hap=hl.agg.count_where(x[1].max_p_chet < SAME_HAP_THRESHOLD)
-                        )
+        row_counts=hl.flatten([
+            hl.array(
+                hl.agg.group_by(
+                    vp_mt.pop,
+                    hl.struct(
+                        csq=csq,
+                        af=af,
+                        # TODO: Review this
+                        # These will only kept the worst csq -- now maybe it'd be better to keep either
+                        # - the worst csq for chet or
+                        # - the worst csq for both chet and same_hap
+                        n_chet=hl.agg.count_where((vp_mt[af].get(1) == csq_i) & (vp_mt[af].get(2, 9) >= csq_i) & (vp_mt[af].get(3, 9) >= csq_i)),
+                        n_same_hap=hl.agg.count_where((vp_mt[af].get(2) == csq_i) & (vp_mt[af].get(1, 9) > csq_i) & (vp_mt[af].get(1, 9) >= csq_i)),
+                        n_unphased=hl.agg.count_where((vp_mt[af].get(3) == csq_i) & (vp_mt[af].get(1, 9) > csq_i) & (vp_mt[af].get(2, 9) > csq_i))
                     )
-                ),
-                hl.array(vp_mt.counts)
+                )
+            ).filter(
+                lambda x: (x[1].n_chet > 0) | (x[1].n_same_hap > 0) | (x[1].n_unphased > 0)
+            ).map(
+                lambda x: x[1].annotate(
+                    pop=x[0]
+                )
             )
-        )
+            for csq_i, csq in enumerate(CSQ_CODES)
+            for af in ['af_gt_0_001', 'af_le_0_001']
+        ])
     ).rows()
 
     gene_ht = gene_ht.explode('row_counts')
     gene_ht = gene_ht.select(
-        **gene_ht.row_counts[0],
-        **gene_ht.row_counts[1]
+        **gene_ht.row_counts
     )
 
     gene_ht.describe()
-
-    # vp_mt = vp_mt.group_rows_by(
-    #     'gene_id'
-    # ).aggregate_rows(
-    #     gene_symbol=hl.agg.take(vp_mt.gene_symbol, 1)[0]
-    # ).aggregate(
-    #     _lof_counts_0_01=get_entry_agg(vp_mt, hl.literal({'lof'}), 0.01),
-    #     _lof_counts_0_001=get_entry_agg(vp_mt, hl.literal({'lof'}), 0.001),
-    #     _damaging_missense_counts_0_01=get_entry_agg(vp_mt, hl.literal({'lof', 'damaging_missense'}), 0.01),
-    #     _damaging_missense_counts_0_001=get_entry_agg(vp_mt, hl.literal({'lof', 'damaging_missense'}), 0.001),
-    #     _missense_counts_0_01=get_entry_agg(vp_mt, hl.literal({'lof', 'damaging_missense', 'missense_variant'}), 0.01),
-    #     _missense_counts_0_001=get_entry_agg(vp_mt, hl.literal({'lof', 'damaging_missense', 'missense_variant'}), 0.001),
-    #     _synonymous_counts_0_01=get_entry_agg(vp_mt, hl.literal({'synonymous_variant'}), 0.01),
-    #     _synonymous_counts_0_001=get_entry_agg(vp_mt, hl.literal({'synonymous_variant'}), 0.001)
-    # ) #.checkpoint('gs://gnomad-tmp/vp_gene.mt', overwrite=overwrite) # TODO: Check whether this is actually harming performance
-    #
-    # gene_ht = vp_mt.annotate_rows(
-    #     **{x[1:]: get_row_agg(vp_mt[x]) for x in vp_mt.entry if x != '_gene_symbol'}
-    # ).rows()
-
     gene_ht = gene_ht.checkpoint(
         'gs://gnomad-lfran/compound_hets/chet_per_gene{}.ht'.format(
             '.chr20' if chr20 else ''
         ),
         overwrite=overwrite
     )
-
 
     gene_ht.flatten().export(
         'gs://gnomad-lfran/compound_hets/chet_per_gene{}.tsv.gz'.format(
@@ -232,7 +201,6 @@ def compute_from_vp_mt(chr20: bool, overwrite: bool):
 
 
 def compute_from_full_mt(chr20: bool, overwrite: bool):
-
     mt = get_gnomad_data('exomes', adj=True, release_samples=True)
     mt = mt.filter_rows(mt.a_index == 1)  # TODO remove when VP_MT has been re-generated
     freq_ht = hl.read_table(annotations_ht_path('exomes', 'frequencies'))
@@ -243,13 +211,7 @@ def compute_from_full_mt(chr20: bool, overwrite: bool):
         mt, freq_ht, vep_ht, rf_ht = filter_to_chr20([mt, freq_ht, vep_ht, rf_ht])
 
     vep_ht = vep_ht.annotate(
-        vep=get_worst_gene_csq_code_expr(
-            vep_ht.vep.annotate(
-                transcript_consequences=vep_ht.vep.transcript_consequences.filter(
-                    lambda tc: (tc.biotype == 'protein_coding')
-                )
-            )
-        ).values()
+        vep=get_worst_gene_csq_code_expr(vep_ht.vep).values()
     )
     mt = mt.annotate_rows(
         popmax=freq_ht[mt.row_key].popmax,
@@ -265,65 +227,89 @@ def compute_from_full_mt(chr20: bool, overwrite: bool):
     mt = mt.explode_rows(mt.vep)
     mt = mt.transmute_rows(**mt.vep)
 
+    # # mt.describe()
+    #
+    # het_het_agg = hl.agg.take(mt.csq, 2, ordering=mt.csq)
     mt = mt.group_rows_by(
         'gene_id'
     ).aggregate_rows(
         gene_symbol=hl.agg.take(mt.gene_symbol, 1)[0]
     ).aggregate(
         counts=hl.agg.group_by(
+            mt.popmax.AF > 0.001,
             hl.struct(
-                snv=hl.is_snp(mt.alleles[0], mt.alleles[1]),
-                af_gt_0_001=mt.popmax.AF > 0.001,
-                csq=mt.csq
-            ),
-            hl.struct(
-                n_hom=hl.agg.count_where(mt.GT.is_hom_var()),
-                n_het=hl.agg.count_where(mt.GT.is_het())
+                hom_csq=hl.agg.filter(mt.GT.is_hom_var(), hl.agg.min(mt.csq)),
+                het_csq=hl.agg.filter(mt.GT.is_het(), hl.agg.min(mt.csq)),
+                het_het_csq=hl.sorted(
+                    hl.array(
+                        hl.agg.filter(mt.GT.is_het(), hl.agg.counter(mt.csq))
+                    ),
+                    key=lambda x: x[0]
+                ).scan(
+                    lambda i, j: (j[0], i[1] + j[1]),
+                    (0, 0)
+                ).find(
+                    lambda x: x[1] > 1
+                )[0]
+                # het_het_csq=hl.agg.take(mt.csq, 2, ordering=mt.csq) -- this runs out of mem :(
+                # het_het_csq=hl.agg.filter(mt.GT.is_het(), hl.or_missing(hl.len(het_het_agg)>1, het_het_agg[1]))
             )
         )
     )
 
     mt = mt.annotate_entries(
-        counts=mt.counts.keys().map(
-            lambda k: (
-                k,
-                get_group_to_counts_expr(k, mt.counts).fold(
-                    lambda i,j: hl.struct(
-                        n_hom=i.n_hom+j.n_hom,
-                        n_het=i.n_het+j.n_het
-                    ),
-                    hl.struct(n_hom=0, n_het=0)
-                )
-            )
+        counts=hl.struct(
+            af_gt_0_001=hl.struct(
+                hom_csq=hl.min(mt.counts.get(True).hom_csq, mt.counts.get(False).hom_csq),
+                het_csq=hl.min(mt.counts.get(True).het_csq, mt.counts.get(False).het_csq),
+                het_het_csq=hl.min(
+                    mt.counts.get(True).het_het_csq,
+                    mt.counts.get(False).het_het_csq,
+                    hl.or_missing(
+                        hl.is_defined(mt.counts.get(True).het_csq) & hl.is_defined(mt.counts.get(False).het_csq),
+                        hl.max(mt.counts.get(True).het_csq, mt.counts.get(False).het_csq)
+                    )
+                ),
+                # het_het_csq=hl.min(het_het_expr(mt.counts.get(True).het_het_csq), af_le_0_001_expr.het_het_csq)
+            ),
+            af_le_0_001=mt.counts.get(False)
         )
     )
 
+    mt = mt.checkpoint('gs://gnomad-tmp/test_2.mt', overwrite=True)
+
     gene_ht = mt.annotate_rows(
-        row_counts=hl.array(
-            hl.agg.explode(
-                lambda x:
+        row_counts=hl.flatten([
+            hl.array(
                 hl.agg.group_by(
-                    x[0].annotate(
-                        pop=mt.meta.pop,
-                        csq=hl.literal(list(CSQ_CODES.keys()))[x[0].csq]
-                    ),
+                    mt.meta.pop,
                     hl.struct(
-                        n_hom=hl.agg.count_where(x[1].n_hom > 0),
-                        n_het=hl.agg.count_where(x[1].n_het == 1),
-                        n_het_het=hl.agg.count_where(x[1].n_het > 1)
+                        csq=csq,
+                        af=af,
+                        n_hom=hl.agg.count_where(mt.counts[af].hom_csq == csq_i),
+                        n_het=hl.agg.count_where(mt.counts[af].het_csq == csq_i),
+                        n_het_het=hl.agg.count_where(mt.counts[af].het_het_csq == csq_i)
                     )
-                ),
-                hl.array(mt.counts)
+                )
+            ).filter(
+                lambda x: (x[1].n_het > 0) | (x[1].n_hom > 0) | (x[1].n_het_het > 0)
+            ).map(
+                lambda x: x[1].annotate(
+                    pop=x[0]
+                )
             )
-        )
+            for csq_i, csq in enumerate(CSQ_CODES)
+            for af in ['af_gt_0_001', 'af_le_0_001']
+        ])
     ).rows()
 
     gene_ht = gene_ht.explode('row_counts')
     gene_ht = gene_ht.select(
         'gene_symbol',
-        **gene_ht.row_counts[0],
-        **gene_ht.row_counts[1]
+        **gene_ht.row_counts
     )
+
+    gene_ht.describe()
 
     gene_ht = gene_ht.checkpoint(
         'gs://gnomad-lfran/compound_hets/het_and_hom_per_gene{}.ht'.format(
