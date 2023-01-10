5e6393b83d5da6d8c607145f58e9e84991fd1ac4
Sarah Stenton <93785543+slstenton@users.noreply.github.com>
2023-01-09 16:00:17 -0800

    Update create_per_gene_matrix.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- create_per_gene_matrix.py
+++ create_per_gene_matrix.py
@@ -150,6 +150,7 @@ def get_cum_csq_map() -> hl.expr.DictExpression:
     """
     Get expression mapping a CSQ code index to all cumulative CSQs that it belongs to.
     
+
     For example, 0 (lof) is included in the following cumulative consequences:
         - lof
         - strong_revel_missense_or_worse
