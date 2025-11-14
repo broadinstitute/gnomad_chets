fd1d6a457e232d33b5ff280b6f5d125c20488d77
Rachel Ungar <raungar@Rachels-MacBook-Air.local>
2025-11-14 16:47:02 -0500

    get file checking working

--- v4/create_vp_matrix.py
+++ v4/create_vp_matrix.py
@@ -1,15 +1,37 @@
 import argparse
-from
+from gnomad.utils.file_utils import file_exists
+import logging
+import sys
 
-#create 
+
 def main(args):
     tmp_dir=args.tmp_dir
+    overwrite=args.overwrite
+    name=args.name
     data_type = 'exomes' if args.exomes else 'genomes'
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-    hl.init(log="{tmp_dir}/hail_vp_matrix.log")
+    #generate just the list of possible variant pairs
+    if args.create_vp_list:
+        outfile=f"{tmp_dir}/{data_type}_{name}_list.ht"
+        if not overwrite:
+            if file_exists(outfile):
+                logger.info(f"{outfile} already exists, exiting program.")
+                sys.exit(0)
+            else:
+                logger.info(f"{outfile} does not exist, running create_vp_list.")
+        else:
+            logger.info(f"overwrite is set to True, running create_vp_list.")
+                
+                
 
+#The order this should be run in is first create_vp_list (or vp_list_by_chrom), then create_full_vp, then create_vp_summary.
 
 if __name__ == "__main__":    
+    
+    # Argument parsing for exomes or genomes, testing, and tmp-dir.
     parser = argparse.ArgumentParser()
     data_grp = parser.add_mutually_exclusive_group(required=True)
     data_grp.add_argument(
@@ -27,6 +49,20 @@ if __name__ == "__main__":
         '--tmp-dir',
         required=True,
         help='Temporary directory for intermediate files.'
+    )
+    parser.add_argument(
+        '--create_vp_list',
+        action='store_true',
+        help='first create just the list of possible variant pairs.'
+    )
+    parser.add_argument(
+        '--overwrite',
+        action='store_true',
+        help='Whether to overwrite existing files.')
+    parser.add_argument(
+        '--name',
+        required=True,
+        help='unique name to be used in file naming.'
     )
 
     args = parser.parse_args()
