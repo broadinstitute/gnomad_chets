a23e23ee60fc4d5b7131a3f4c7662ab940c076de
Rachel Ungar <raungar@Rachels-MacBook-Air.local>
2026-01-05 13:43:43 -0500

    skeleton for phasing script, does not currently run

---
+++ v4/phase_gnomad.py
@@ -0,0 +1,139 @@
+import hail as hl
+import logging
+import timeit
+import argparse
+
+
+logging.basicConfig(
+    format="%(asctime)s (%(name)s %(lineno)s): %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+logger = logging.getLogger("phase_gnomad")
+logger.setLevel(logging.INFO)
+
+
+def get_em_expr(gt_counts):
+    hap_counts = hl.experimental.haplotype_freq_em(gt_counts)
+    return hl.bind(
+        lambda x: hl.struct(
+            hap_counts=x,
+            p_chet=(x[1] * x[2]) / (x[0] * x[3] + x[1] * x[2])
+        ),
+        hap_counts
+    )
+
+def get_em_expressions(gt_counts):
+    return dict(
+        em=hl.struct(
+            raw=get_em_expr(gt_counts.raw),
+            adj=get_em_expr(gt_counts.adj),
+        ),
+        em_plus_one=hl.struct(
+            raw=get_em_expr(gt_counts.raw + [0, 0, 0, 0, 1, 0, 0, 0, 0]),
+            adj=get_em_expr(gt_counts.adj + [0, 0, 0, 0, 1, 0, 0, 0, 0]),
+        )
+    )
+def get_phased_gnomad_ht(
+        ht: hl.Table
+) -> hl.Table:
+    if isinstance(ht.gt_counts, hl.expr.DictExpression):
+        ht = ht.select(
+            phase_info=ht.gt_counts.map_values(
+                lambda pop_count: hl.bind(
+                    lambda x: hl.struct(
+                        gt_counts=x,
+                        **{
+                            k: v for f in get_em for k, v in f(x).items()
+                        }
+                    ),
+                    hl.struct(
+                        raw=pop_count.raw.map(lambda y: hl.int32(y)),
+                        adj=pop_count.adj.map(lambda z: hl.int32(z))
+                    )
+                )
+            )
+        )
+    # exploded
+    else:
+        ht = ht.annotate(
+            **{
+                k: v for f in get_em for k, v in f(ht.gt_counts).items()
+            }
+        )
+
+    return ht
+
+def main(args):
+    start = timeit.default_timer()
+    tmp_dir = args.tmp_dir
+    overwrite = args.overwrite
+    output_postfix = args.output_postfix or ""
+    data_type = args.data_type
+    test = args.test
+    
+    hl.init(
+        log="/create_vp_matrix.log",
+        tmp_dir=tmp_dir,
+    )
+    
+    logger.info(
+        f"""
+        Running script with the following parameters:
+
+            Data type: {data_type}
+            Test: {test}
+            Output postfix: {output_postfix}
+            Overwrite: {overwrite}
+            Tmp dir: {tmp_dir}
+            Least consequence: {least_consequence}
+            Max freq: {max_freq}
+        """
+    )
+    
+    if args.phase:
+        #phase variant pairs
+        ht=get_phased_gnomad_ht(ht)
+        
+        #write phased data
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--tmp-dir",
+        default=DEFAULT_TMP_DIR,
+        help="Temporary directory for intermediate files.",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Filter to PCNT gene (chr21:46324141-46445769) for testing purposes.",
+    )
+    parser.add_argument(
+        "--output-postfix",
+        help=(
+            'Postfix to append to output file names (e.g., "pcnt_test" for files like '
+            "exomes.vp_list.pcnt_test.ht)."
+        ),
+    )
+    parser.add_argument(
+        "--overwrite", action="store_true", help="Whether to overwrite existing files."
+    )
+    parser.add_argument(
+        "--data-type",
+        default=DEFAULT_DATA_TYPE,
+        choices=DATA_TYPE_CHOICES,
+        help=(
+            f'Data type to use. Must be one of {", ".join(DATA_TYPE_CHOICES)}. Default '
+            f"is {DEFAULT_DATA_TYPE}.",
+        ),
+    )
+    parser.add_argument(
+        "--phase",
+        action="store_true",
+        help="Whether to phase variant pairs.",
+        
+    
+    
+    args = parser.parse_args()
+    main(args)
